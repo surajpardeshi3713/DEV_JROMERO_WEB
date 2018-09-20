ea4281c314dd7fc7e2e976a58aa77fc82550702f
jhonub <n.bjhon@gmail.com>
2018-09-20 10:42:26 -0500

    primer avance de header

---
+++ Plantillas_Web/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Blog Personal</title>
+    <!-- link de ukit css -->
+    <link rel="stylesheet" type="text/css" href="./uikit/css/uikit.min.css">
+    <link rel="stylesheet" type="text/css" href="./uikit/css/uikit-customizer-theme.css">
+
+    <!-- link de google font -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <!-- link de estilo css -->
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+
+    <!--comenzando la cabezera -->
+
+    <nav class="uk-navbar">
+      <div class="uk-container uk-container-center">
+        <a href="" class="uk-navbar-brand">
+          <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    	    width="70px" height="70px" viewBox="0 0 530 410" style="enable-background:new 0 0 530 410;" xml:space="preserve">
+          <style type="text/css">
+          	.Sombra_x0020_paralela{fill:none;}
+          	.NeÃ_x00B3_n_x0020_azul_x0020_grueso{fill:none;stroke:#0073BC;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;}
+          	.Agua_x0020_iluminada{fill:url(#SVGID_1_);stroke:#FFFFFF;stroke-width:0.25;stroke-miterlimit:1;}
+          	.Resalte_x0020_en_x0020_negro_x0020_{fill:url(#SVGID_2_);stroke:#FFFFFF;stroke-width:0.3629;stroke-miterlimit:1;}
+          	.Coco_GS{fill-rule:evenodd;clip-rule:evenodd;fill:#FFDD00;}
+          </style>
+          <g>
+          	<path d="M193.4,280.8c0,34-28.6,61.7-63.9,61.7s-63.9-27.7-63.9-61.7v-10.2H1v10.2c0,34.3,14.4,65.4,37.6,87.8
+          		C61.9,391.1,94,405,129.5,405c71,0,128.5-55.6,128.5-124.2V5h-64.7V280.8z"/>
+          	<path d="M353.1,226.8h54.4c65,0,117.9-49.8,117.9-110.9C525.3,54.7,472.4,5,407.4,5h-97.7h-39.2h-1.2v67.2h1.2v0h136.9
+          		c25.6,0,46.4,19.6,46.4,43.7c0,24.1-20.8,43.7-46.4,43.7H269.3v0.1v23.4v18v39.8v0.2l163.4,163.8l-0.1,0.1h0.3h34.5h22.2H528v-2.9
+          		L353.1,226.8z"/>
+          </g>
+          </svg>
+        </a>
+        <div class="uk-navbar-flip">
+            <ul class="uk-navbar-nav">
+              <li class="uk-active"><a href="">Sobre Mi</a></li>
+              <li><a href="">Blog</a></li>
+              <li><a href="">Portafolio</a></li>
+              <li><a href="">Contacto</a></li>
+            </ul>
+
+           <!--util para movil mas tarde
+          <div class="uk-navbar-content uk-navbar-center">...
+          </div>
+          <a href="" class="uk-navbar-toggle"></a>
+        -->
+        </div>
+      </div>
+    </nav>
+    <!-- terminando la cabezera-->
+    
+    <!-- link de JQuery-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- link ukit js -->
+    <script type="text/javascript" src="./uikit/js/uikit.min.js"></script>
+  </body>
+</html>
